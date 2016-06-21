85507c02c87901ed958790ee33697f9cd6c37949
Akshayf444 <mohite.akshay118@gmail.com>
2016-06-21 18:43:16 +0530

    Changed Login Page

--- application/views/User/login.php
+++ application/views/User/login.php
@@ -1,77 +1,81 @@
-<div class="container" style="margin-top: 2em;">
-    <?php
-    if (isset($message)) {
-        echo '<div class="alert alert-danger" style="text-align:center">' . $message . '</div>';
-    }
-    ?>
-    <div class="row">
-        <div class="col-lg-6 col-md-6 col-sm-8 col-lg-offset-3 col-md-offset-3 col-sm-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading" style="text-align: center">
-                    <img  src="<?php echo asset_url() ?>images/youngdoctor.png" width="45%" >
-                </div>
-                <div class="panel-body">
-                    <?php echo form_open('User/index') ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="images/favicon.png" type="image/png">
+        <?php $link = "http://www.udisha.co.in/" ?>
+        <title>Superchem</title>
+        <link href="<?php echo $link; ?>css/site.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $link; ?>css/style.default.css" rel="stylesheet">
+        <script src="<?php echo $link; ?>js/jquery.js" type="text/javascript"></script>
+        <script src="<?php echo $link; ?>js/miniNotification.js" type="text/javascript"></script>
+        <script src="<?php echo $link; ?>js/bootstrap.js" type="text/javascript"></script>
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="<?php echo $link; ?>js/html5.js"></script>
+        <script src="<?php echo $link; ?>js/respond.js"></script>
+        <![endif]-->
+    </head>
 
-                    <div class="form-group">
+    <body class="signin">
+        <header>
+        </header>
+        <section>
+            <div class="signinpanel">
+                <div class="pull-right">Helpline No : <span class="helpline">022-65657701</span><br>From 10 am - 6 pm</div>
+                <h2 class="hidden-xs" ><strong>Welcome to Superchem</strong></h2>
+                <div class="row">
+                    <div class="col-md-6" style="padding-top: 3em;">
+                        <div class="signin-info">
+                            <div class="logopanel">
+                                <img src="<?php echo asset_url(); ?>images/youngdoctor.png" width="100%">
+                            </div>
+                        </div><!-- signin0-info -->
+
+                    </div><!-- col-sm-7 -->
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5">
+
+                        <?php echo form_open('User/index'); ?>
+                        <h4 class="nomargin">Sign In</h4>
+                        <p class="mt5 mb20">Login to access your account.</p>
+
                         <input type="text" class="form-control uname" placeholder="Mobile" name="mobile"/>
-                    </div>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <input type="password" class="form-control pword" placeholder="Password" id="pwd" aria-describedby="basic-addon2" name="password">
-                            <span class="input-group-addon" id="eye"><i class="fa fa-eye"></i></span>
-                        </div>
-                    </div>
-                    <!--                        <div class="col-lg-8">
-                                                <input type="password" id="pwd" class="form-control pword" placeholder="Password" name="password" /></div><div class="col-lg-4">  <button type="button" id="eye">Show</button></div>
-                                        </div>-->
-                    <input class="btn btn-success btn-block" type="submit" value="Sign In" >
+                        <input type="password" class="form-control pword" placeholder="Password" name="password" />
+    <!--                    <a href="#"><small>Forgot Your Password?</small></a>-->
+                        <button class="btn btn-success btn-block" type="submit" name="submit" style="background: #2A5567">Sign In</button>
 
-                    </form>
+                        </form>
+                    </div><!-- col-sm-5 -->
+
+                </div><!-- row -->
+                <hr style="margin-top: 7em">
+                <div >
+                    <div class="pull-left">
+                        &copy; 2016. All Rights Reserved. 
+                    </div>
+                    <div class="pull-right">
+                        Powered By: <a href="http://techvertica.com/" target="_blank">Techvertica</a>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
 
-    <script>
+            </div><!-- signin -->
 
-        $(function () {
+        </section>
 
-
-            $('#button').click(function () {
-
-                $('#password').password('toggle');
-
-            });
-
-        });
-
-    </script>
-
-    <script>
-
-        $(function () {
-            function show() {
-                var p = document.getElementById('pwd');
-                p.setAttribute('type', 'text');
-            }
-
-            function hide() {
-                var p = document.getElementById('pwd');
-                p.setAttribute('type', 'password');
-            }
-
-            var pwShown = 0;
-
-            document.getElementById("eye").addEventListener("click", function () {
-                if (pwShown == 0) {
-                    pwShown = 1;
-                    show();
-                } else {
-                    pwShown = 0;
-                    hide();
+        <script>
+            $(function () {
+                $('#mini-notification').miniNotification({closeButton: true, closeButtonText: 'x'});
+                function blinker() {
+                    $('.helpline').fadeOut(500);
+                    $('.helpline').fadeIn(500);
                 }
-            }, false);
-        });
 
-    </script>
+                setInterval(blinker, 1000);
+            });
+        </script>
+    </body>
+</html>
