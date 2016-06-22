04f4fe64487f97c28cb2e69a3b109d2ebfe9728d
sadhnakaushik <sadhnasharma34@gmail.com>
2016-06-22 11:54:39 +0530

    brand list

--- application/views/template3.php
+++ application/views/template3.php
@@ -80,9 +80,11 @@
                         <li>
                             <a href="<?php echo site_url('User/Division'); ?>"><i class="fa fa-dashboard"></i> Division</a>
                         </li>
+                     <?php   if( $this->type==1){ ?>
                         <li>
                             <a href="<?php echo site_url('User/CompanyList');       ?>"><i class="fa fa-dashboard"></i> Company</a>
                         </li>
+                     <?php } ?>
                         <li>
                             <a href="<?php //echo site_url('User/brandList');       ?>"><i class="fa fa-dashboard"></i> Bonus Offer</a>
                         </li>
