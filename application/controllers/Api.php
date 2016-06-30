6eba4b8a639e52833172a8429d3b85a3eba6be16
Akshayf444 <mohite.akshay118@gmail.com>
2016-06-30 12:18:31 +0530

    Added Bonus Id in getBonusOffer

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -333,6 +333,8 @@ class Api extends MY_Controller {
         } elseif ($type === 'continuous') {
             $condition[] = 'starting_days < 0 AND  ending_days > 30 AND ending_days > 0';
             $order_by = " ORDER BY bf.ending_days ASC ";
+        } elseif ($type === 'company') {
+            $order_by = " ORDER BY bf.ending_days DESC,cm.company_name ASC ";
         } else {
             $order_by = " ORDER BY bf.ending_days DESC ";
         }
@@ -351,7 +353,7 @@ class Api extends MY_Controller {
             $condition[] = "brand_id = {$product_id} ";
         }
 
-        if ($type === '') {
+        if ($type === '' || $type === 'company') {
             $totalCount = $this->Bonus->countBonus($condition);
             $totalCount = $totalCount->bonusCount;
             $paging = $this->calculatePaging($perpage, $totalCount, $page);
@@ -422,6 +424,7 @@ class Api extends MY_Controller {
                     'strength' => $item->strength,
                     'packing' => $item->packing,
                     'mrp' => $item->mrp,
+                    'bonus_id' => $item->bonus_id,
                 );
             }
 
